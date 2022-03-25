5bfef6598171f529b14c935eddb34977ff5a84d0
Ricardo Muñoz Montiel <parker82812@gmail.com>
2022-03-24 22:11:19 -0600

    Index cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@
 $('.burger').on('click', function(event){
   $('.navigation-bar').slideToggle('200');
 })
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
